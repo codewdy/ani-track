acab849467efd24ff3fa0c6ddbaea51622f4e437
codewdy <codewdy@gmail.com>
2025-08-24 08:12:01 +0800

    add searchlist cache

--- server/ani_list_searcher/searcher_list.py
+++ server/ani_list_searcher/searcher_list.py
@@ -1,4 +1,10 @@
 from ani_list_searcher.web_searcher.searcher_list import searcher_list as web_searcher_list
+from functools import cache
 
+@cache
 def searcher_list():
     return web_searcher_list()
+
+@cache
+def searcher_dict():
+    return {i.name: i for i in searcher_list()}
