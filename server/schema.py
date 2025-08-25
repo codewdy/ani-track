d61bf85282f351619f61c798e7696e668890cdb7
codewdy <codewdy@gmail.com>
2025-08-25 22:33:24 +0800

    add schema

---
+++ server/schema.py
@@ -0,0 +1,60 @@
+import datetime
+from pydantic import BaseModel
+from enum import Enum
+from typing import Optional, List, Dict
+from datetime import datetime
+
+
+class DownloadStatus(Enum):
+    Running = "running"
+    Finished = "finished"
+    Failed = "failed"
+
+
+class Episode(BaseModel):
+    name: str
+    url: str
+    filename: str
+    download_status: DownloadStatus
+    download_error: Optional[str] = None
+
+
+class Channel(BaseModel):
+    name: str
+    url: str
+    dirname: str
+    episodes: List[Episode]
+    tracking: bool
+    latest_update: datetime
+
+
+class AnimationStatus(Enum):
+    Wanted = "wanted"
+    Watching = "watching"
+    Watched = "watched"
+    Dropped = "dropped"
+
+
+class Animation(BaseModel):
+    name: str
+    dirname: str
+    channels: List[Channel]
+    current_channel: int
+    status: AnimationStatus
+
+
+class AnimationDB(BaseModel):
+    animations: Dict[int, Animation]
+    next_id: int
+
+
+class ResourceConfig(BaseModel):
+    root_dir: str
+    sub_dirs: List[str]
+    current_sub_dir: str
+
+
+class Config(BaseModel):
+    tmp_dir: str
+    resource: ResourceConfig
+    animationdb: str
