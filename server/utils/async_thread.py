7d72e2dc3ec17f160f3d7ab0a04705e917b1b843
codewdy <codewdy@gmail.com>
2025-08-27 22:57:34 +0800

    fix async thread

--- server/utils/async_thread.py
+++ server/utils/async_thread.py
@@ -10,21 +10,20 @@ class AsyncThread:
 
     def start(self):
         self._event = threading.Event()
-        self._thread = threading.Thread(
-            target=lambda: asyncio.run(self.main_loop()))
+        self._thread = threading.Thread(target=self.thread_main)
         self._thread.start()
         self._event.wait()
+
+    def thread_main(self):
+        asyncio.run(self.main_loop())
+        self._loop.run_forever()
 
     async def main_loop(self):
         await self.init()
         self._loop = asyncio.get_running_loop()
         self._event.set()
-        await self.loop()
 
     async def init(self):
-        pass
-
-    async def loop(self):
         pass
 
     @staticmethod
