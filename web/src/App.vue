33185d08bf18dd3400b82bf0cca73938f5df9caa
codewdy <codewdy@gmail.com>
2025-09-07 21:11:23 +0800

    fix some app

--- web/src/App.vue
+++ web/src/App.vue
@@ -10,9 +10,6 @@
           <router-view />
         </n-layout>
       </n-layout>
-      <n-layout-footer position="absolute" style="height: 64px; padding: 24px" bordered>
-        城府路
-      </n-layout-footer>
     </n-layout>
   </n-message-provider>
 </template>
