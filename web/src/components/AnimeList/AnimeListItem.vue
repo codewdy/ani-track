f2f32be54d7682de843826cb9aa22054aca281d3
codewdy <codewdy@gmail.com>
2025-09-10 08:51:19 +0800

    add animelist

--- web/src/components/AnimeList/AnimeListItem.vue
+++ web/src/components/AnimeList/AnimeListItem.vue
@@ -1,50 +1,41 @@
 <template>
-  <n-card>
-    <template #cover>
-      <img :src="item.cover" class="card-cover">
-    </template>
-    <n-h2 class="card-name"> {{ item.name }}</n-h2>
-    <n-h3 class="card-fullname"> {{ item.fullname }} </n-h3>
+  <n-space>
+    <n-card v-for="item in animations">
+      <template #cover>
+        <img :src="item.icon_url" class="card-cover">
+      </template>
+      <n-h2 class="card-name">
+        <a :href="'/anime-view/' + item.animation_id">{{ item.name }}</a>
+      </n-h2>
 
-    <p class="card-watch"> 观看： {{ item.watched }} / {{ item.downloaded }} / {{ item.total }}</p>
-  <n-dropdown trigger="hover" :options="options" @select="handleSelect">
-    <n-button>在看</n-button>
-  </n-dropdown>
-</n-card>
+      <p class="card-watch"> 观看： {{ item.watched_episode }} / {{ item.total_episode }}</p>
+      <n-dropdown trigger="hover" :options="options" @select="(status) => set_status(item.animation_id, status)">
+        <n-button>{{ watch_status[item.status] }}</n-button>
+      </n-dropdown>
+    </n-card>
+  </n-space>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import { NH2, NH3, NCard, NDropdown, NButton } from 'naive-ui'
+import { ref, inject } from 'vue'
+import { NH2, NCard, NDropdown, NButton, NSpace } from 'naive-ui'
+import { watch_status, all_watch_status } from '@/lib/schema'
 
+const animations = inject('animations')
 
-const item = ref({
-    name: "动画名称",
-    fullname: "动画全名",
-    cover: "https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg",
-    watched: 10, downloaded: 20, total: 30,
-    status: "在看",
-});
+const options = all_watch_status.map(status => ({
+  label: watch_status[status],
+  key: status
+}));
 
-
-const options = [
-{
-    label: '想看',
-    key: 'atlantis nahamas, nassau'
-},
-{
-    label: '在看',
-    key: 'marina bay sands',
-},
-{
-    label: '看过',
-    key: 'brown\'s hotel, london'
-},
-{
-    label: '搁置',
-    key: 'the beverly hills hotel, los angeles'
+function set_status(animation_id, status) {
+  // TODO: Send request to server
+  animations.value.forEach(item => {
+    if (item.animation_id == animation_id) {
+      item.status = status
+    }
+  })
 }
-];
 
 </script>
 
@@ -53,16 +44,22 @@ const options = [
 .n-card {
   max-width: 200px;
 }
+
 .card-name {
-    margin:10px 0px 0px 0px;
+  margin: 10px 0px 0px 0px;
 }
+
 .card-fullname {
-    margin:0px 0px 0px 0px;
+  margin: 0px 0px 0px 0px;
 }
+
 .card-watch {
-    margin:0px 0px 10px 0px;
+  margin: 0px 0px 10px 0px;
 }
+
 .card-cover {
-width: 100%; aspect-ratio: 3 / 4; object-fit: cover;
+  width: 100%;
+  aspect-ratio: 3 / 4;
+  object-fit: cover;
 }
 </style>
---
+++ web/src/lib/schema.js
@@ -0,0 +1,13 @@
+export const all_watch_status = [
+    "watching",
+    "wanted",
+    "watched",
+    "dropped",
+]
+
+export const watch_status = {
+    watching: "在看",
+    wanted: "想看",
+    watched: "看完",
+    dropped: "搁置",
+}
