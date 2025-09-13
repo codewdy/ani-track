1f0f6df8ba82e48af8266829defe8f507d07fbe7
codewdy <codewdy@gmail.com>
2025-09-13 19:56:16 +0800

    add animation info

--- web/src/components/AnimeView/AnimeView.vue
+++ web/src/components/AnimeView/AnimeView.vue
@@ -1,5 +1,12 @@
 <template>
     <n-space vertical>
+        <NH1> {{ info?.name }} </NH1>
+        <n-space>
+            <n-tag v-for="(item, index) in episodes" @click="changeEpisode(index)" :checked="index == episode_idx"
+                checkable>
+                {{ item.name }}
+            </n-tag>
+        </n-space>
         <VideoPlayer v-if="episodes.length > 0" :url="episodes[episode_idx]?.url ?? ''"
             v-model:current_time="current_time" :time="time" @pause="onPause" @done="onDone" />
     </n-space>
@@ -8,13 +15,14 @@
 <script setup>
 import { onMounted, ref, watch } from 'vue';
 import VideoPlayer from './VideoPlayer.vue';
-import { NButton, NSpace, useMessage } from 'naive-ui';
+import { NH1, NTag, NSpace, useMessage } from 'naive-ui';
 import axios from 'axios';
 import { useRoute } from 'vue-router';
 
 const route = useRoute()
 const message = useMessage()
 
+const info = ref(null)
 const episodes = ref([])
 const episode_idx = ref(0)
 const time = ref(0)
@@ -39,12 +47,22 @@ watch(() => current_time.value, (newTime) => {
     }
 })
 
+function changeEpisode(idx) {
+    if (idx == episode_idx.value) {
+        return
+    }
+    latest_update_time = 0
+    episode_idx.value = idx
+    time.value = 0
+    updateWatched(episode_idx.value, 0)
+}
+
 function onPause() {
     updateWatched(episode_idx.value, current_time.value)
 }
 
 function onDone() {
-    latest_update_time = -100
+    latest_update_time = 0
     episode_idx.value++
     time.value = -1
     updateWatched(episode_idx.value, 0)
@@ -56,6 +74,7 @@ function reload(animation_id) {
     }).catch(err => {
         message.error("获取动画信息失败: " + err.message)
     }).then(res => {
+        info.value = res.data
         episodes.value = res.data.episodes
         episode_idx.value = res.data.watched_episode
         time.value = res.data.watched_episode_time
